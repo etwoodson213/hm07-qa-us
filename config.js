50f9055978bb075f27b21f3be510f1ad63a01ce3
_woodson_ <etwoodson213@gmail.com>
2024-08-13 02:19:06 -0400

    delete - passed all test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-24d1d3b4-6802-41b4-b61f-8031ef9f992f.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-90213fe6-640e-41fa-8572-5e188bffc54f.containerhub.tripleten-services.com"
 }
--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,16 +1,43 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('Deleting a kit...', async () => {
-	let kitDeleted;
+const requestBody = {
+	"ok": true
+};
+
+test('Should return status 200...', async () => {
+	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/6`, {
 			method: 'DELETE',
 		});
-		kitDeleted = response.status
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
 	expect(actualStatusCode).toBe(200);
+});
 
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                  Request Body                                  ||
+// ! ||--------------------------------------------------------------------------------||
+
+test('Body should contain...', async () => {
+	let actualResponseBody;
+
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/6`, {
+			method: 'DELETE',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+	
+	expect(actualResponseBody.ok).toBe(true);
 });
